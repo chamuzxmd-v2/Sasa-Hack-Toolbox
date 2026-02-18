3e8561a1107f85bfc1ce5fd52a0a14f12df894de
chamuzxmd-v2 <chamuzxmd@gmail.com>
2026-02-18 17:09:11 +0530

    Create tools.py

---
+++ tools.py
@@ -0,0 +1,35 @@
+# tools.py
+
+TOOLS = {
+"OSINT": {
+"1": ("IP Info Tool", "https://github.com/ipinfo/ipinfo-cli"),
+"2": ("TheHarvester", "https://github.com/laramies/theHarvester"),
+"3": ("Phone Number OSINT", "https://github.com/sundowndev/phoneinfoga"),
+"4": ("Username Search", "https://github.com/sherlock-project/sherlock"),
+"5": ("Email OSINT", "https://github.com/megadose/holehe"),
+},
+
+"NETWORK": {
+"6": ("Port Scanner", "https://github.com/sullo/nikto"),
+"7": ("Ping Tool", "https://github.com/BlaskLee/fastping"),
+"8": ("Nmap Scripts", "https://github.com/nmap/nmap"),
+"9": ("Network Monitor", "https://github.com/wireshark/wireshark"),
+"10": ("Speed Test", "https://github.com/sivel/speedtest-cli"),
+},
+
+"WEB": {
+"11": ("Web Scanner", "https://github.com/wapiti-scanner/wapiti"),
+"12": ("Dir Bruteforce", "https://github.com/maurosoria/dirsearch"),
+"13": ("SQL Scanner", "https://github.com/sqlmapproject/sqlmap"),
+"14": ("XSS Scanner", "https://github.com/s0md3v/XSStrike"),
+"15": ("Web Fuzzer", "https://github.com/ffuf/ffuf"),
+},
+
+"DEV": {
+"16": ("GitHub Downloader", "https://github.com/aria2/aria2"),
+"17": ("Python Projects", "https://github.com/vinta/awesome-python"),
+"18": ("Linux Tools", "https://github.com/tldr-pages/tldr"),
+"19": ("Code Snippets", "https://github.com/30-seconds/30-seconds-of-code"),
+"20": ("AI Tools List", "https://github.com/mahseema/awesome-ai-tools"),
+}
+}
